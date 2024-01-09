f48e84802af2fff403e686684c0569714d852cf1
Mara Caoile <mara.caoile@gmail.com>
2024-01-08 21:29:53 -0600

    set up homepage html and light css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import Link from 'next/link';
 import { useAuth } from '../utils/context/authContext';
 
 function Home() {
@@ -7,18 +6,26 @@ function Home() {
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
+      id="homeContainer"
     >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <h1>Welcome, {user.fbUser.displayName} </h1>
+      <div className="options">
+        <Link passHref href="/orders">
+          <div className="option-box-1">
+            <h2 className="index-h2">View Orders</h2>
+          </div>
+        </Link>
+        <Link href="/orders/new" passHref>
+          <div className="option-box-2">
+            <h2 className="index-h2">Create New Order</h2>
+          </div>
+        </Link>
+        <Link href="/revenue" passHref>
+          <div className="option-box-3">
+            <h2 className="index-h2">View Revenue</h2>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
